5327ad95d3d68fe9be06ea5e4ee52d61f815455e
wrecklist <gtwrecklist@gmail.com>
2012-09-29 21:05:44 -0400

    first commit

---
+++ your_html.js
@@ -0,0 +1,52 @@
+$('#home_button').css('position','relative').css('left',($(window).width()-42)+'px');
+function simpleIndex(){
+	$.ajax({
+		url: "api/simple",
+		context: document.body,
+		success: function(data){
+			$('#IndexResult').html(data);
+		}
+	});
+}
+function simpleGet(){
+	$.ajax({
+		url: "api/simple/testItemValue",
+		context: document.body,
+		success: function(data){
+			$('#buy').html(data);
+		}
+	});
+}
+function simplePost(){
+	$.ajax({
+		url: "api/simple",
+		data: {'itemValue': 'testItemValue'},
+		context: document.body,
+		type: 'POST',
+		success: function(data){
+			$('#PostResult').html(data);
+		}
+	});
+}
+function simplePut(){
+	$.ajax({
+		url: "api/simple/testItemValue",
+		context: document.body,
+		data: {'itemValue': 'testItemNewValue'},
+		headers: {'X-HTTP-Method-Override': 'PUT'},
+		type: 'POST',
+		success: function(data){
+			$('#PutResult').html(data);
+		}
+	});
+}
+function simpleDelete(){
+	$.ajax({
+		url: "api/simple/testItem",
+		context: document.body,
+		type: 'DELETE',
+		success: function(data){
+			$('#DeleteResult').html(data);
+		}
+	});
+}
