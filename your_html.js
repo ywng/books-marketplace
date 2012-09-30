4ebe9acb3122091db07a3d2ffe7b668383b2948f
wrecklist <gtwrecklist@gmail.com>
2012-09-29 21:36:50 -0400

    arvind: checking in the xml, html, js files for the preliminary version of search

--- your_html.js
+++ your_html.js
@@ -1,52 +1,101 @@
 $('#home_button').css('position','relative').css('left',($(window).width()-42)+'px');
+
+$(function() {
+    console.log('myfunction');
+
+    $("#home").on('pageinit', function(e) {
+        console.log('page create event');
+        $("#searchFieldHome").on("keydown", function(e) {
+            if (e.which == 13) {
+                var text = $(this).val();
+                if(text.length >= 1) {
+                    console.log('Input Search Query:' + text);
+                    
+                    $.ajax({
+                        url: "api/search/" + text.toLowerCase(),
+                        context: document.body,
+                        dataType: "json", 
+                        async: false,
+                        success: function(data, textStatus, jqXHR){
+                            console.log("Object returned by php:" + data);
+                            var str = ""; 
+                            for(var i=0; i < data.length; i++) { 
+                                str += "<li data-theme=\"d\">";
+                                str += "<h2>" + data[i].title + "</h2>";
+                                str += "<p>" + data[i].edition + " by " + data[i].author + "</p>";
+                                str += "<p>" + data[i].numItemsForSale + "  items starting from $" + data[i].startingPrice + "</p>";
+                                str += "</li>";
+                            }
+                            console.log('searchresults:' + str);
+                            console.log('searchtext inside ajax:' + text);
+                            $.mobile.changePage( $("#searchResultsPage") );
+                            $("#searchFieldSERP").val(text);
+                            $("#searchResults").html(str);
+                            $("#searchResults").listview("refresh");
+                        },
+                    
+                        error: function(jqHXR, textStatus, errorThrown) {
+                            console.log('ajaxerror:' +textStatus + ' ' + errorThrown);
+                        }
+
+                    }); // end of the ajax call
+
+                } 
+            }   
+        }); // end of keydown event on searchField in the homepage
+
+    }); // end of pageinit event on homepage
+
+}); //end of main jquery function invocation
+
 function simpleIndex(){
-	$.ajax({
-		url: "api/simple",
-		context: document.body,
-		success: function(data){
-			$('#IndexResult').html(data);
-		}
-	});
+    $.ajax({
+        url: "api/simple",
+        context: document.body,
+        success: function(data){
+            $('#IndexResult').html(data);
+        }
+    });
 }
 function simpleGet(){
-	$.ajax({
-		url: "api/simple/testItemValue",
-		context: document.body,
-		success: function(data){
-			$('#buy').html(data);
-		}
-	});
+    $.ajax({
+        url: "api/simple/testItemValue",
+        context: document.body,
+        success: function(data){
+            $('#buy').html(data);
+        }
+    });
 }
 function simplePost(){
-	$.ajax({
-		url: "api/simple",
-		data: {'itemValue': 'testItemValue'},
-		context: document.body,
-		type: 'POST',
-		success: function(data){
-			$('#PostResult').html(data);
-		}
-	});
+    $.ajax({
+        url: "api/simple",
+        data: {'itemValue': 'testItemValue'},
+        context: document.body,
+        type: 'POST',
+        success: function(data){
+            $('#PostResult').html(data);
+        }
+    });
 }
 function simplePut(){
-	$.ajax({
-		url: "api/simple/testItemValue",
-		context: document.body,
-		data: {'itemValue': 'testItemNewValue'},
-		headers: {'X-HTTP-Method-Override': 'PUT'},
-		type: 'POST',
-		success: function(data){
-			$('#PutResult').html(data);
-		}
-	});
+    $.ajax({
+        url: "api/simple/testItemValue",
+        context: document.body,
+        data: {'itemValue': 'testItemNewValue'},
+        headers: {'X-HTTP-Method-Override': 'PUT'},
+        type: 'POST',
+        success: function(data){
+            $('#PutResult').html(data);
+        }
+    });
 }
 function simpleDelete(){
-	$.ajax({
-		url: "api/simple/testItem",
-		context: document.body,
-		type: 'DELETE',
-		success: function(data){
-			$('#DeleteResult').html(data);
-		}
-	});
+    $.ajax({
+        url: "api/simple/testItem",
+        context: document.body,
+        type: 'DELETE',
+        success: function(data){
+            $('#DeleteResult').html(data);
+        }
+    });
 }
