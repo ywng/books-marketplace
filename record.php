3704f61fa27c5bce58efc040d72d6654ef07e759
wrecklist <gtwrecklist@gmail.com>
2012-10-05 18:20:53 -0400

    Jason: record php for record api

---
+++ record.php
@@ -0,0 +1,130 @@
+<?php
+include_once "db_helper.php";
+
+class pendingRecord
+{
+        public $transID;
+        public $BuySell;
+	public $listingID;
+	public $Price;
+	public $Title;
+	public $counterpart;
+
+}
+
+function fetchRecords($userid) {
+    
+    $userid=mysql_real_escape_string($userid);
+
+//-------------------------fetch pending transaction record-------------------------
+
+//fetch pending sale transaction
+    $dbQuery = "SELECT list.ID, list.Price, Item.Title, User.Name as BuyerName, tran.TransactionID AS transactionID
+FROM Listing as list
+INNER JOIN Transaction as tran ON list.ID = tran.ListingID AND list.SellerID ="
+.$userid.
+" INNER JOIN Item ON list.ItemID = Item.ID
+INNER JOIN Status ON tran.StatusID = Status.ID AND Status.Name = 'in progress'
+INNER JOIN User ON tran.BuyerID = User.ID";
+
+    $dataset = getDBResultsArray($dbQuery,true);
+
+    $resultArray = array();
+    $i = 0;
+   foreach ($dataset as $datarow) {
+       $pendingRecord = new pendingRecord();
+       $pendingRecord->BuySell = "Sell";
+       $pendingRecord->listingID = $datarow["ID"];
+       $pendingRecord->transID = $datarow["transactionID"];
+       $pendingRecord->Price = $datarow["Price"];
+       $pendingRecord->Title = $datarow["Title"];
+       $pendingRecord->counterpart = $datarow["BuyerName"];
+   
+      $resultArray[$i] = $pendingRecord;
+        $i++;
+   }
+
+//fetch pending buy transaction
+      $dbQuery = "SELECT list.ID,  list.Price, Item.Title, User.Name AS SellerName, tran.TransactionID AS transactionID
+FROM Listing AS list
+INNER JOIN Transaction AS tran ON list.ID = tran.ListingID AND tran.BuyerID ="
+.$userid.
+" INNER JOIN Item ON list.ItemID = Item.ID
+INNER JOIN Status ON tran.StatusID = Status.ID AND Status.Name = 'in progress'
+INNER JOIN User ON list.SellerID = User.ID";
+
+
+    $dataset = getDBResultsArray($dbQuery,true);
+
+    foreach ($dataset as $datarow) {
+       $pendingRecord = new pendingRecord();
+       $pendingRecord->BuySell = "Buy";
+       $pendingRecord->listingID = $datarow["ID"];
+       $pendingRecord->transID = $datarow["transactionID"];
+       $pendingRecord->Price = $datarow["Price"];
+       $pendingRecord->Title = $datarow["Title"];
+       $pendingRecord->counterpart = $datarow["SellerName"];
+   
+      $resultArray[$i] = $pendingRecord;
+        $i++;
+   }
+
+//-------------------------fetch past transaction record-------------------------
+     $resultArray[$i] ="Past_Transaction_Record_Starts";
+     $i++;
+    $dbQuery = "SELECT list.ID, list.Price, Item.Title, User.Name as BuyerName, tran.TransactionID AS transactionID
+FROM Listing as list
+INNER JOIN Transaction as tran ON list.ID = tran.ListingID AND list.SellerID ="
+.$userid.
+" INNER JOIN Item ON list.ItemID = Item.ID
+INNER JOIN Status ON tran.StatusID = Status.ID AND Status.Name = 'completed'
+INNER JOIN User ON tran.BuyerID = User.ID";
+
+    $dataset = getDBResultsArray($dbQuery,true);
+
+    foreach ($dataset as $datarow) {
+       $pendingRecord = new pendingRecord();
+       $pendingRecord->BuySell = "Sell";
+       $pendingRecord->listingID = $datarow["ID"];
+       $pendingRecord->transID = $datarow["transactionID"];
+       $pendingRecord->Price = $datarow["Price"];
+       $pendingRecord->Title = $datarow["Title"];
+       $pendingRecord->counterpart = $datarow["BuyerName"];
+   
+      $resultArray[$i] = $pendingRecord;
+        $i++;
+   }
+
+      $dbQuery = "SELECT list.ID,  list.Price, Item.Title, User.Name AS SellerName, tran.TransactionID AS transactionID
+FROM Listing AS list
+INNER JOIN Transaction AS tran ON list.ID = tran.ListingID AND tran.BuyerID ="
+.$userid.
+" INNER JOIN Item ON list.ItemID = Item.ID
+INNER JOIN Status ON tran.StatusID = Status.ID AND Status.Name = 'completed'
+INNER JOIN User ON list.SellerID = User.ID";
+
+
+    $dataset = getDBResultsArray($dbQuery,true);
+
+    foreach ($dataset as $datarow) {
+       $pendingRecord = new pendingRecord();
+       $pendingRecord->BuySell = "Buy";
+       $pendingRecord->listingID = $datarow["ID"];
+       $pendingRecord->transID = $datarow["transactionID"];
+       $pendingRecord->Price = $datarow["Price"];
+       $pendingRecord->Title = $datarow["Title"];
+       $pendingRecord->counterpart = $datarow["SellerName"];
+   
+      $resultArray[$i] = $pendingRecord;
+        $i++;
+   }
+
+    
+
+//-------------------------return result-----------------------------------------
+    header("Content-type: application/json");
+    echo json_encode($resultArray);
+
+}
+
+?>
