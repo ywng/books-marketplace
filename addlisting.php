0114106bdff63c81e9aebdf42fe6e6558feeb032
wrecklist <gtwrecklist@gmail.com>
2012-10-06 20:52:52 -0400

    arvind: additional tagging for item on sell listing page

--- addlisting.php
+++ addlisting.php
@@ -11,7 +11,7 @@ function addListing($data) {
         $GLOBALS["_PLATFORM"]->sandboxHeader('HTTP/1.1 401 Unauthorized');
         die();
     }
-    
+
     $userObj = getUser($_USER['uid']);    
     
     $dbQuery = "INSERT INTO Listing (SellerID, ItemID, CreationDate, ExpirationDate, Price, Quantity, `Condition`, Status)
@@ -22,6 +22,19 @@ function addListing($data) {
                         .", '".$data->condition."', 4);";
 
     $result = getDBResultInserted($dbQuery,'listingid');
+    
+    if (!empty($data->newtags)) {
+        $tagArray = array_unique(preg_split('/[^[:alnum:]]+/', $data->newtags));
+        
+        foreach ($tagArray as $tag) {
+            if(strlen($tag) > 0) {
+                $dbQuery = "INSERT INTO Category (ItemID, Tag)
+                             VALUES(".$data->itemid
+                            .", '".$tag."');";
+                getDBResultInserted($dbQuery,'itemtagid');
+            }
+        }
+    }
 
     header("Content-type: application/json");
     echo json_encode($result); 
